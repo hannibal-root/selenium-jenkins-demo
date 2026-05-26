ae71da929c71cb1d2faccbfe03f8800a601480c5
gkisvarga <gabor.kisvarga@blackbelt.hu>
2026-05-26 16:55:29 +0200

    chrome

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -21,6 +21,8 @@ public class ExampleTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--window-size=1920,1080");
 
         driver = new ChromeDriver(options);
     }
