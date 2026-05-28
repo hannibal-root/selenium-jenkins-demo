3dcc30cbfc9e109485cc654ec323bb21e62a6ac0
rappbea <rappbea@gmail.com>
2026-05-28 20:04:28 +0200

    Bea_Test

--- src/test/java/TC3_OwnProfileData_Test.java
+++ src/test/java/TC3_OwnProfileData_Test.java
@@ -7,18 +7,32 @@ import static org.hamcrest.Matchers.equalTo;
 public class TC3_OwnProfileData_Test extends BaseAPI_Test {
 
     /**
-     * Ez a teszt kiírja a profil adatait JSON formátumban, amivel a regisztráció létrejött
-     * <p>
+     * Ez a teszt kiírja a profil adatait JSON formátumban,
+     * amivel a regisztráció létrejött
+     *
      * GET /api/v1/user/find
-     * <p>
+     *
      * getUserByUsername
      */
     @Test
     public void TC3_OwnProfileData() {
-        /* A globalTestData.properties fájlból kiolvassuk az e-mail címet.*/
-        // String userName = globalTestData.getProperty(Consts.REG_EMAILADDRESS);
 
-        /* A regisztrációs adatoknál megadott e-mail címet felhasználva megkeressük a profilt és kiíratjuk vele a profil adatokat.*/
+        String userName = "admin@demo.io";
 
+        Response response =
+
+                given()
+                        .header(AUTH_HEADER, "Bearer " + authToken)
+                        .queryParam("username", userName)
+
+                        .when()
+                        .get("/api/v1/user/find");
+
+        response.prettyPrint();
+
+        response.then()
+                .statusCode(200);
+
+        logger.info("User profile data successfully loaded");
     }
 }
--- src/test/java/TC6_GetAllUsers_Test.java
+++ src/test/java/TC6_GetAllUsers_Test.java
@@ -1,4 +1,3 @@
-
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
@@ -6,12 +5,21 @@ import static io.restassured.RestAssured.given;
 
 public class TC6_GetAllUsers_Test extends BaseAPI_Test {
 
-    /**
-     * Ez a teszt listázza az összes regisztrált felhasználó adatait.
-     * <p>
-     * GET /api/v1/users
-     * <p>
-     * getAllUsers
-     */
+    @Test
+    public void getAllUsers_Test() {
 
+        Response response =
+
+                given()
+                        .header("Authorization", "Bearer " + authToken)
+
+                        .when()
+                        .get("/api/v1/users");
+
+        response.prettyPrint();
+
+        response.then().statusCode(200);
+
+        logger.info("Status code is 200");
+    }
 }
