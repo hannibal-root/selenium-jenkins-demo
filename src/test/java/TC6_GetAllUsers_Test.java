57d2b142ec98970510c5339af1736e46e9ccfd5e
gkisvarga <gabor.kisvarga@blackbelt.hu>
2026-05-28 18:23:33 +0200

    TC6

---
+++ src/test/java/TC6_GetAllUsers_Test.java
@@ -0,0 +1,17 @@
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class TC6_GetAllUsers_Test extends BaseAPI_Test {
+
+    /**
+     * Ez a teszt listázza az összes regisztrált felhasználó adatait.
+     * <p>
+     * GET /api/v1/users
+     * <p>
+     * getAllUsers
+     */
+
+}
