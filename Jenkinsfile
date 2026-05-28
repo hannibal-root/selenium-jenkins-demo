e8dbd6e242047d698478bf5382bea617f520aaa0
buntabence <buntabence@gmail.com>
2026-05-28 19:02:59 +0200

    TC6 update 1

--- Jenkinsfile
+++ Jenkinsfile
@@ -11,7 +11,7 @@ pipeline {
 
         stage('Checkout') {
             steps {
-                git branch: 'main',
+                git branch: 'buntabence',
                     url: 'https://github.com/hannibal-root/selenium-jenkins-demo.git'
             }
         }
--- src/test/java/TC6_GetAllUsers_Test.java
+++ src/test/java/TC6_GetAllUsers_Test.java
@@ -1,8 +1,13 @@
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
 
 public class TC6_GetAllUsers_Test extends BaseAPI_Test {
 
@@ -14,4 +19,22 @@ public class TC6_GetAllUsers_Test extends BaseAPI_Test {
      * getAllUsers
      */
 
+    @Test
+    @Story("Get all user data")
+    @Description("...")
+    public void testGetUsers() {
+
+        Response response =given()
+                .contentType(JSON)
+                .header(AUTH_HEADER, "Bearer " + authToken)
+
+                .when()
+                .get("/api/v1/users");
+
+                response.prettyPrint();
+
+                response.then()
+                .statusCode(200)
+                .log().all();
+    }
 }
