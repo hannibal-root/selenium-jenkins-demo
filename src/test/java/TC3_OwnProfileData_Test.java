8003f191b477865b66c7554db119232faf3ec399
buntabence <buntabence@gmail.com>
2026-05-28 19:55:23 +0200

    TC3 update 1

--- src/test/java/TC3_OwnProfileData_Test.java
+++ src/test/java/TC3_OwnProfileData_Test.java
@@ -1,7 +1,9 @@
 import io.restassured.response.Response;
+import org.apache.http.Consts;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.equalTo;
 
 public class TC3_OwnProfileData_Test extends BaseAPI_Test {
@@ -17,8 +19,23 @@ public class TC3_OwnProfileData_Test extends BaseAPI_Test {
     public void TC3_OwnProfileData() {
         /* A globalTestData.properties fájlból kiolvassuk az e-mail címet.*/
         // String userName = globalTestData.getProperty(Consts.REG_EMAILADDRESS);
+        String userName = "jsmith";
 
         /* A regisztrációs adatoknál megadott e-mail címet felhasználva megkeressük a profilt és kiíratjuk vele a profil adatokat.*/
+
+        Response response =
+                given()
+                        .contentType(JSON)
+                        .header(AUTH_HEADER, "Bearer " + authToken)
+                        .queryParam("username", userName)
+
+                        .when()
+                        .get("/api/v1/user/find");
+
+        response.then()
+                .statusCode(200)
+                .body("username", equalTo(userName))
+                .log().all();
 
     }
 }
